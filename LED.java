7ac98fad69e897f74fb342d27daa95248c0415ab
CodeWizDurham <durham@thecodewiz.com>
2024-04-13 11:48:36 -0700

    Update LED.java

---
+++ LED.java
@@ -0,0 +1,5 @@
+//Set LED colors based on Arm Position
+//Placement-Green (180-280)
+//Pickup-Blue (40-50) Degrees
+//Stowed- Slow Flashing Yellow (85-95) Degrees
+//Broken-RED (0-39)(51-84)(96-179)(281-360)
